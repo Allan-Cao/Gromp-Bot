6fe71f1479bbb06d1067ac5fa63487bb1e8dd3f8
Allan Cao <allan@allancao.ca>
2023-05-18 20:47:59 -0600

    add download button and clean up bayes_helper

--- bayes_helper.py
+++ bayes_helper.py
@@ -16,8 +16,8 @@ class BayesGame:
             self.name = ""
         else:
             self.name = game_name[0]
-        self.team1 = game["teamTriCodes"][0]
-        self.team2 = game["teamTriCodes"][1]
+        self.team1_code = game["teamTriCodes"][0]
+        self.team2_code = game["teamTriCodes"][1]
         self.blockName = game["blockName"]
         self.subBlockName = game["subBlockName"]
         self.status = game["status"]
@@ -27,15 +27,16 @@ class BayesGame:
 
     @property
     def game_finished(self) -> bool:
-        return self.status == "FINISHED" or self.status == "ENDED"
-
+        return self.status in ["FINISHED", "ENDED"]
+    
     @property
     def rofl_available(self) -> bool:
-        return "ROFL_REPLAY" in self.assets or "SCRIM_REPLAY" in self.assets
+        return any(asset in ["ROFL_REPLAY", "SCRIM_REPLAY"] for asset in self.assets)
 
     @property
     def teams(self) -> str:
-        return f"{self.team1} vs {self.team2}"
+        return f"{self.team1_code} vs {self.team2_code}"
+
 
     @property
     def local_timestring(self) -> int:
@@ -46,44 +47,46 @@ class BayesScrim(BayesGame):
         self.createdAt = game.get("createdAt")
         self.status = game.get("status")
         self.game_id = game.get("id")
-        self.team1 = game["teams"][0]["code"]
-        if len(game["teams"]) > 1:
-            self.team2 = game["teams"][1]["code"]
-        else:
-            self.team2 = "Unknown"
-        if game["replayAvailable"]:
-            self.assets = ["SCRIM_REPLAY"]
-        else:
-            self.assets = []
+        self.team1_code = game["teams"][0]["code"]
+        self.team2_code = game["teams"][1]["code"] if len(game["teams"]) > 1 else "Unknown"
+        self.assets = ["SCRIM_REPLAY"] if game["replayAvailable"] else []
         self.game_name = game["name"]
-        
+
 def get_matches(querystring: Optional[dict]) -> list:
     token = get_token()
-    response = requests.get(
-        "https://lolesports-api.bayesesports.com/emh/v1/games",
-        headers={"Authorization": f"Bearer {token}"},
-        params=querystring,
-    )
-
-    if response.status_code != 200:
-        return []
-    else:
+    try:
+        response = requests.get(
+            "https://lolesports-api.bayesesports.com/emh/v1/games",
+            headers={"Authorization": f"Bearer {token}"},
+            params=querystring,
+        )
+        response.raise_for_status()
         return response.json()["games"]
+    except (requests.exceptions.RequestException, ValueError):
+        return []
 
-def get_icons():
+
+def get_icons() -> dict:
+    token = get_token()
+    try:
+        response = requests.get(
+            "https://lolesports-api.bayesesports.com/historic/v1/riot-lol/leagues",
+            headers={"Authorization": f"Bearer {token}"},
+        )
+        response.raise_for_status()
+        return {tournament["name"]: tournament["logoUrl"] for tournament in response.json()}
+    except (requests.exceptions.RequestException, ValueError):
+        return {}
+
+
+def get_tags() -> list[str]:
     token = get_token()
     response = requests.get(
-        'https://lolesports-api.bayesesports.com/historic/v1/riot-lol/leagues',
-        headers={"Authorization": f"Bearer {token}"},
-    ).json()
-    return {tournament['name']: tournament['logoUrl'] for tournament in response}
-
-def get_tags():
-    token = get_token()
-    return requests.get(
         "https://lolesports-api.bayesesports.com/emh/v1/tags",
         headers={"Authorization": f"Bearer {token}"},
-    ).json()
+    )
+    return response.json() if response.ok else []
+
 
 def get_scrim_games(querystring: Optional[dict]) -> list:
     # Gets the latest 20 scrim games
@@ -93,48 +96,28 @@ def get_scrim_games(querystring: Optional[dict]) -> list:
         headers={"Authorization": f"Bearer {token}"},
         params=querystring,
     )
-    if response.status_code != 200:
-        return []
-    else:
-        return response.json()["games"]
+    return response.json()["games"] if response.ok else []
 
-def get_asset(
-    gameId: str, asset_type: Optional[str] = "ROFL_REPLAY"
-) -> Optional[str]:
+
+def get_asset(game_id: int | str, asset_type: Optional[str] = "ROFL_REPLAY") -> Optional[str]:
     token = get_token()
+    querystring = {}
     if asset_type == "SCRIM_REPLAY":
-        response = requests.get(
-            f"https://lolesports-api.bayesesports.com/scrim/v1/games/{gameId}/downloadRiotReplay",
-            headers={"Authorization": f"Bearer {token}"},
-        )
+        url = f"https://lolesports-api.bayesesports.com/scrim/v1/games/{game_id}/downloadRiotReplay"
     else:
-        querystring = {"gameId": gameId, "type": asset_type}
-        response = requests.get(
-            f"https://lolesports-api.bayesesports.com/emh/v1/games/{gameId}/download",
-            headers={"Authorization": f"Bearer {token}"},
-            params=querystring,
-        )
-    if response.status_code != 200:
-        return None
-    else:
-        return response.json()["url"]
+        querystring.update({"gameId": game_id, "type": asset_type})
+        url = f"https://lolesports-api.bayesesports.com/emh/v1/games/{game_id}/download"
+    response = requests.get(url, headers={"Authorization": f"Bearer {token}"}, params=querystring)
+    return response.json()["url"] if response.ok else None
 
-def get_asset_link(gameId: str) -> str:
-    # Scrim gameId's are integers, so we can check if it's a scrim game
-    # There could be a chance though that match games have integers as their gameId
-    if gameId.isnumeric():
-        match_link = get_asset(gameId, "SCRIM_REPLAY")
-        if match_link is None:
-            match_link = get_asset(gameId, "ROFL_REPLAY")
-            if match_link is None:
-                return "Game not found"
-            else:
-                return match_link
-        else:
-            return match_link
+
+def get_asset_link(game_id: str) -> str:
+    if game_id.isnumeric():
+        asset_types = ["SCRIM_REPLAY", "ROFL_REPLAY"]
     else:
-        match_link = get_asset(gameId, "ROFL_REPLAY")
-        if match_link is None:
-            return "No replay could be found"
-        else:
-            return match_link
+        asset_types = ["ROFL_REPLAY"]
+    for asset_type in asset_types:
+        asset_link = get_asset(game_id, asset_type)
+        if asset_link is not None:
+            return asset_link
+    return "Game not found" if type(game_id) == int else "No replay could be found"
--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from discord.interactions import Interaction
 from bayes_helper import (
     get_tags,
     get_asset_link,
@@ -17,42 +18,42 @@ import logging
 from datetime import datetime
 from dotenv import load_dotenv
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("bot.log"),
+        logging.StreamHandler(),
+    ],
+)
 
 load_dotenv()
 
 discord_token = os.environ.get("DISCORD_TOKEN")
 if discord_token is None:
     raise ValueError("Discord token not set.")
+guilds_allowed = os.environ.get("GUILDS_ALLOWED")
+if guilds_allowed is None:
+    guild_ids = []
+else:
+    guild_ids = list(map(int, guilds_allowed.split(",")))
 
 bot = discord.Bot(help_command=commands.MinimalHelpCommand())
 
 TAGS = get_tags()
 ICONS = get_icons()
 
+class DownloadButton(discord.ui.Button):
+    def __init__(self, *args, **kwargs):
+        super().__init__(label="Download Replay", *args, **kwargs)
 
-@bot.event
-async def on_ready():
-    await bot.change_presence(
-        status=discord.Status.online,
-        activity=discord.Game("with data"),
-    )
-    print(f"We have logged in as {bot.user}")
-
-
-@bot.slash_command(guild_ids=[1074844803310833785, 1044416171736309940])
-async def tournaments(ctx, search: Optional[str]) -> Optional[list]:
-    if search is None:
-        await ctx.respond("\n".join(TAGS[0:10]))
-    else:
-        possible_tags = process.extract(search, TAGS, limit=5)
-        if len(possible_tags) == 0:
-            await ctx.respond("No matching Tournaments were found")
-        else:
-            await ctx.respond(
-                "\n".join([tournament[0] for tournament in possible_tags])
-            )
-
+    async def callback(self, interaction: discord.Interaction):
+        current_embed = interaction.message.embeds[0]
+        game_id = next((page.value for page in current_embed.fields if page.name == 'Game ID'), None)
+        if game_id is None:
+            return await interaction.response.send_message("Unable to find the game ID.")
+        asset_link = get_asset_link(game_id)
+        await interaction.response.send_message(asset_link)
 
 def create_pages(matches: list[BayesGame] | list[BayesScrim]) -> list:
     pages = []
@@ -61,10 +62,13 @@ def create_pages(matches: list[BayesGame] | list[BayesScrim]) -> list:
             if isinstance(game, BayesScrim):
                 embed = discord.Embed(title=game.teams)
             elif isinstance(game, BayesGame):
-                embed = discord.Embed(
-                    title=game.teams,
-                    description=f"{game.blockName} | {game.subBlockName}",
-                )
+                if game.blockName is None and game.subBlockName is None:
+                    embed = discord.Embed(title=game.teams)
+                else:
+                    embed = discord.Embed(
+                        title=game.teams,
+                        description=f"{game.blockName} | {game.subBlockName}",
+                    )
                 icon_tag = max(
                     [process.extractOne(tag, list(ICONS.keys())) for tag in game.tags],
                     key=lambda x: x[1],
@@ -86,10 +90,11 @@ def create_pages(matches: list[BayesGame] | list[BayesScrim]) -> list:
                 name="Lord Grompulus Kevin Ribbiton of Croaksworth Bot",
                 icon_url="https://static.wikia.nocookie.net/leagueoflegends/images/8/8b/Gromp_Render.png",
             )
-
+            
             embed.set_footer(
                 text=f"Generated with data from Bayes Esports by Gromp Bot • {datetime.now().strftime('%b %d %Y • %H:%M')} "
             )
+                
             pages.append(
                 Page(
                     embeds=[embed],
@@ -97,46 +102,56 @@ def create_pages(matches: list[BayesGame] | list[BayesScrim]) -> list:
             )
     return pages
 
+@bot.event
+async def on_ready():
+    await bot.change_presence(
+        status=discord.Status.online,
+        activity=discord.Game("with data"),
+    )
+    logging.info(f"We have logged in as {bot.user}")
+    print(f"We have logged in as {bot.user}")
 
-@bot.slash_command(guild_ids=[1074844803310833785, 1044416171736309940])
-async def match(
-    ctx, tournament: Optional[str], team1: Optional[str], team2: Optional[str]
-):
-    querystring = {}
-    if team1 is not None:
-        querystring["team1"] = team1
-    if team2 is not None:
-        querystring["team2"] = team2
+@bot.slash_command(guild_ids=guild_ids)
+async def tournaments(ctx, search: Optional[str]):
+    if search is None:
+        tags = TAGS[:10]
+    else:
+        possible_tags = process.extract(search, TAGS, limit=5)
+        tags = [tournament[0] for tournament in possible_tags] if possible_tags else ["No matching Tournaments were found"]
+    await ctx.respond("\n".join(tags))
+
+
+
+@bot.slash_command(guild_ids=guild_ids)
+async def match(ctx, tournament: Optional[str], team1: Optional[str], team2: Optional[str]):
+    querystring = {"team1": team1, "team2": team2}
     if tournament is not None:
         most_likely_tournament = process.extractOne(tournament, TAGS)
-        print(most_likely_tournament)
-        querystring["tags"] = most_likely_tournament
+        querystring["tags"] = most_likely_tournament[0] if most_likely_tournament else None
     pages = create_pages([BayesGame(game) for game in get_matches(querystring)])
-    if len(pages) > 0:
-        paginator = Paginator(pages=pages)
-        await paginator.respond(ctx.interaction, ephemeral=False)
-    else:
-        await ctx.respond("No matches were found.")
+    if len(pages) == 0:
+        await ctx.respond("No matches found")
+        return
+    view = discord.ui.View()
+    view.add_item(DownloadButton())
+    paginator = Paginator(pages=pages, custom_view=view)
+    await paginator.respond(ctx.interaction)
 
 
-@bot.slash_command(guild_ids=[1074844803310833785, 1044416171736309940])
+@bot.slash_command(guild_ids=guild_ids)
 async def scrim(ctx, lobby_name: Optional[str], enemy_team: Optional[str]):
-    querystring = {}
-    if lobby_name is not None:
-        querystring["gameName"] = lobby_name
+    querystring = {"gameName": lobby_name} if lobby_name else {}
     if enemy_team is not None:
-        querystring["teamCodes"] = [enemy_team]
+        querystring["teamCodes"] = enemy_team
     pages = create_pages([BayesScrim(game) for game in get_scrim_games(querystring)])
-    if len(pages) > 0:
-        paginator = Paginator(pages=pages)
-        await paginator.respond(ctx.interaction, ephemeral=False)
-    else:
-        await ctx.respond("No scrims were found.")
+    view = discord.ui.View()
+    view.add_item(DownloadButton())
+    paginator = Paginator(pages=pages, custom_view=view)
+    await paginator.respond(ctx.interaction, ephemeral=len(pages) == 0)
 
 
-@bot.slash_command(guild_ids=[1074844803310833785, 1044416171736309940])
+@bot.slash_command(guild_ids=guild_ids)
 async def download(ctx, game_id: str):
     await ctx.respond(get_asset_link(game_id))
-
 
 bot.run(discord_token)
